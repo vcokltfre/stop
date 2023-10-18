a746f991d8e199a0c17c06b56526f858245d6d1f
vcokltfre <vcokltfre@gmail.com>
2023-10-18 16:27:25 +0100

    feat: add stub command handling to main

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: %s <run|build|explain> <file>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "run":
+		fmt.Println("Not implemented yet")
+		os.Exit(1)
+	case "build":
+		fmt.Println("Not implemented yet")
+		os.Exit(1)
+	case "explain":
+		fmt.Println("Not implemented yet")
+		os.Exit(1)
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+}
